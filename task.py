f779551707c5c4b590acc8bd0f1c0febaf869e4a
00019323 <00019323@wiut.uz>
2023-11-16 15:24:49 +0500

    updated plot

--- task.py
+++ task.py
@@ -32,4 +32,47 @@ print(dataset.columns.tolist())
 # Displaying a sample of the dataset
 print("\nSample Data:")
 print(dataset.head(1000))
+def plot_columns(data, col_list, plot_type, n_col=2, **kwargs):
+    n_row = len(col_list) // n_col + 1
+    plt.figure(figsize=(n_col * 5, n_row * 4))  # Adjust the size as needed
 
+    for i, col in enumerate(col_list):
+        ax = plt.subplot(n_row, n_col, i + 1)
+
+        if plot_type == "hist":
+            sns.histplot(data=data, x=col, multiple="stack", **kwargs)
+
+        elif plot_type == "bar":
+            sns.barplot(data=data, x=col, **kwargs)
+
+        elif plot_type == "count":
+            sns.countplot(data=data, x=col, **kwargs)
+
+        elif plot_type == "box":
+            sns.boxplot(data=data, x=col, **kwargs)
+
+        elif plot_type == "line":
+            x = kwargs.get('x', None)
+            sns.lineplot(data=data, x=x, y=col, ax=ax, **kwargs)
+
+        elif plot_type == "trend":
+            # Assuming plot_trend is a defined function
+            x = kwargs.get('x', None)
+            plot_trend(data=data, x=x, y=col, ax=ax, **kwargs)
+
+        elif plot_type == "top":
+            # Assuming 'top' is defined
+            top = kwargs.get('top', 5)
+            temp = data[col].value_counts()
+            if top > 0:
+                sns.barplot(x=temp.index[0:top], y=temp[0:top])
+            else:
+                sns.barplot(x=temp.index[-1:top:-1], y=temp[-1:top:-1])
+
+        else:
+            raise ValueError(f"Invalid plot_type argument: {plot_type}")
+
+        ax.set_title(f"Distribution of {col}")
+
+    plt.tight_layout()
+    #plt.show()
